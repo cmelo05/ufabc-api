123d2b0924c5a167a5caaf44c6cea972cd69d217
Caio Melo <ccm1092@hotmail.com>
2015-12-03 09:53:19 -0200

    SRC folder created

---
+++ src/br/edu/ufabc/ufabcapi/server/Server.scala
@@ -0,0 +1,11 @@
+package br.edu.ufabc.ufabcapi.server
+
+/**
+ * Created by: Caio Cezar de Melo - RA: 11102010
+ * 
+ * This method is responsible for keeping the server running. It receives all the HTTP requests and process
+ * them according to the path selected.
+ */
+object Server extends App {
+
+}
