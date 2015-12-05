3d47c3c92431f5eb58e737aa0c904922964e3231
Caio Melo <ccm1092@hotmail.com>
2015-12-05 18:21:22 -0200

    Novas funcoes adicionadas

--- src/main/scala/br/edu/ufabc/ufabcapi/models/Disciplina.scala
+++ src/main/scala/br/edu/ufabc/ufabcapi/models/Disciplina.scala
@@ -11,12 +11,42 @@ package main.scala.br.edu.ufabc.ufabcapi.models
     "periodo": "2",
     "conceito": "B"
  */
-class Disciplina (val codigo: String, val ano: Int, val disciplina: String, val categoria: String,
-                  val creditos: Int, val situacao: String, val periodo: Int, val conceito: String ) {
+trait Disciplina {
+  
+  def codigo: String
+  def ano: Int
+  def disciplina: String
+  def categoria: String
+  def creditos: Int
+  def situacao: String
+  def periodo: String
+  def conceito: String
   
   /**
- 	* Override to return a CSV model
+ 	* Override para retonar uma linha de CSV
  	*/
   override def toString(): String = codigo+";"+ano+";"+disciplina+";"+categoria+";"
   +creditos+";"+situacao+";"+periodo+";"+conceito     
+  
+    /**
+   * Entrada: Conceito do aluno em String; Creditos da disciplina em Int
+   * Saida: Float dessa operacao
+   * 
+   * Metodo utiliza o Pattern Matching para retornar o calculo correto
+   */
+  def CalculoUnitario(): Float ={
+    val conceitoA: Int = 4
+    val conceitoB: Int = 3
+    val conceitoC: Int = 2
+    val conceitoD: Int = 1
+    val conceitoReprova: Int = 0
+    
+    this.conceito match{
+      case "A" => conceitoA * this.creditos
+      case "B" => conceitoB * this.creditos
+      case "C" => conceitoC * this.creditos
+      case "D" => conceitoD * this.creditos
+      case _ => conceitoReprova * this.creditos
+    }
+  }
 }
--- src/main/scala/br/edu/ufabc/ufabcapi/server/ServerAPI.scala
+++ src/main/scala/br/edu/ufabc/ufabcapi/server/ServerAPI.scala
@@ -6,26 +6,27 @@ import spray.routing.SimpleRoutingApp
 import spray.json.DefaultJsonProtocol
 import spray.httpx.unmarshalling._
 import spray.httpx.marshalling._
+import main.scala.br.edu.ufabc.ufabcapi.models._
 
 /**
- * Created by: Caio Cezar de Melo - RA: 11102010
- * 
- * This method is responsible for keeping the server running. It receives all the HTTP requests and process
- * them according to the path selected.
+ * Classe usada para manipular dados JSON de disciplina
  */
-
-case class Disciplina (val codigo: String, val ano: Int, val disciplina: String, val categoria: String,
-                  val creditos: Int, val situacao: String, val periodo: String, val conceito: String ) {
-  
-  /**
- 	* Override to return a CSV model
- 	*/
-  override def toString(): String = codigo+";"+ano.toString()+";"+disciplina+";"+categoria+
-  ";"+creditos.toString()+";"+situacao+";"+periodo+";"+conceito     
+case class DisciplinaObject (override val codigo: String, override val ano: Int, override val disciplina: String,
+    override val categoria: String, override val creditos: Int, override val situacao: String,
+    override val periodo: String, override val conceito: String ) extends Disciplina{
 }
 
+
+/**
+ * Criado por: Caio Cezar de Melo - RA: 11102010
+ * 
+ * Esse metodo e responsavel por manter o servidor rodando. Ele recebe todos os requests HTTP e processa de acordo
+ * com o caminho selecionado
+ * 
+ */
+
 object DisciplinaProtocol extends DefaultJsonProtocol {
-  implicit val disciplinaFormat = jsonFormat8(Disciplina)
+  implicit val disciplinaFormat = jsonFormat8(DisciplinaObject)
 }
 
 object ServerAPI extends App with SimpleRoutingApp {
@@ -33,7 +34,8 @@ object ServerAPI extends App with SimpleRoutingApp {
   import spray.httpx.SprayJsonSupport._
   implicit val actorSystem = ActorSystem()
   
-  startServer(interface = "localhost", port = 50501) {   
+  startServer(interface = "localhost", port = 50501) {  
+    println("O servidor foi iniciado")
       path("hello"){
         get {
         complete {
@@ -43,12 +45,93 @@ object ServerAPI extends App with SimpleRoutingApp {
     }~   
        path("test_end_point") {
          put {
-        entity(as[Array[Disciplina]]) { disciplinas =>
+        entity(as[Array[DisciplinaObject]]) { disciplinas =>
         disciplinas.foreach {println}
         complete(disciplinas.toString())
       }
     }
+  }~
+  path("retornacr"){
+    post{
+      entity(as[Array[DisciplinaObject]]) { disciplinas =>
+        complete(RetornaCR(disciplinas).toString())
+      }
+    }
+  }~
+  path("retornaca"){
+    post{
+      entity(as[Array[DisciplinaObject]]) { disciplinas =>
+        complete(RetornaCA(disciplinas).toString())
+      }
+    }
   }
   }
+  
+  /**
+   * Entrada: Array de Disciplinas
+   * Saida: Float com o CR do aluno
+   * 
+   * O metodo declara os inteiros relacionados ao conceitos e efetua a 
+   * operacao de calculo de CR atraves da chamada do Calculo Unitario de CR
+   */
+  def RetornaCR(disciplinas: Array[DisciplinaObject]): Float ={
+    println("Operacao de calcular o CR foi solicitada")
+    
+    var cr: Float = 0
+    var creditosCursados: Int = 0
+    
+    for(dis <- disciplinas){
+      println(dis.toString())
+      cr += dis.CalculoUnitario()   
+      creditosCursados += dis.creditos
+    }
+    
+    cr /= creditosCursados
+    
+    cr
+  }
+  
+   /**
+   * Entrada: Array de Disciplinas
+   * Saida: Float com o CA do aluno
+   * 
+   * O metodo declara os inteiros relacionados ao conceitos e efetua a 
+   * operacao de calculo de CA apenas das disciplinas em que foi aprovado atraves da chamada do CalculoUnitario
+   */
+  def RetornaCA(disciplinas: Array[DisciplinaObject]): Float ={
+    println("Operacao de calcular o CA foi solicitada")
+    
+    var ca: Float = 0
+    var creditosCursados: Int = 0
+    
+    for(dis <- disciplinas){
+      dis.situacao match{
+        case "Aprovado" => {
+          ca += dis.CalculoUnitario()
+          creditosCursados += dis.creditos
+        }
+        case "Reprovado" => {}
+        case "Repr.Freq" => {}
+      }
+    }
+    
+    ca /= creditosCursados
+    
+    ca
+  }
+  
+  def RetornaCSV(disciplinas: Array[DisciplinaObject]): String = {
+    println("Operacao de retonar CSV foi solicitada")
+    var teste: String = "" 
+
+    for(dis <- disciplinas){
+      StringBuilder sb = new StrinbBuilder();
+      sb.
+    }
+     teste.addString(dis.toString())
+   
+  }
+  
+
 
 }
